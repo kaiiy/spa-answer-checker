643731d454ba7514bc545af002ed05c9b22394be
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-08-20 00:47:15 +0900

    wip: 2023-08-20 00:47:15+09:00

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { obfuscator } from 'rollup-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/spa-answer-checker",
-  plugins: [react()],
+  plugins: [react(), obfuscator(
+    {
+      stringArray: true,
+      stringArrayEncoding: ['rc4'],
+      stringArrayThreshold: 1,
+      transformObjectKeys: true,
+    }
+  )],
 })
